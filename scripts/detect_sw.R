4063c75e4c0793c6002ac746cafecd903b58b165
brandonleekramer <brandonleekramer@gmail.com>
2021-07-16 09:10:32 -0400

    pushing for weekly updates

--- scripts/detect_sw.R
+++ scripts/detect_sw.R
@@ -181,7 +181,7 @@ detect_blockchain_sw <- function(df, id, input, sum_only = FALSE, prob = FALSE){
   
   if( sum_only == TRUE ){
     
-    df <- df %>% select(-starts_with("app_"))
+    df <- df %>% select(-app_blockchain, app_cryptocurrency)
     
   } else { df }
   
@@ -334,7 +334,245 @@ detect_business_sw <- function(df, id, input, sum_only = FALSE, prob = FALSE){
 }
 
 
+# detect_statprog_sw classifies statistical programming languages  
 
+detect_prog_stat_sw <- function(df, id, input, sum_only = FALSE, prob = FALSE){
+  
+  library("dplyr")
+  library("readr")
+  library("tidyr")
+  library("readr")
+  library("stringr")
+  library("data.table")
+  library("tidytext")
+  library("tidytable")
+  
+  # load dictionaries for top-50 languages based on: 
+  # https://madnight.github.io/githut/#/pull_requests/2021
+  
+  # top statistical languages 
+  prog_python <- get_dictionary_terms(sub_type = "Python")
+  prog_scala <- get_dictionary_terms(sub_type = "Scala")
+  prog_rlang <- get_dictionary_terms(sub_type = "R")
+  prog_matlab <- get_dictionary_terms(sub_type = "MATLAB")
+  prog_stata <- get_dictionary_terms(sub_type = "Stata")
+  prog_sas <- get_dictionary_terms(sub_type = "SAS")
+  prog_julia <- get_dictionary_terms(sub_type = "Julia")
+  
+  id <- enquo(id)
+  input <- enquo(input)
+  
+  df <- df %>% 
+    as_tidytable() %>% 
+    tidytable::mutate.("{{input}}" := tolower({{ input }})) %>% 
+    detect_types(!!id, !!input, prog_python) %>% 
+    detect_types(!!id, !!input, prog_scala) %>% 
+    detect_types(!!id, !!input, prog_rlang) %>% 
+    detect_types(!!id, !!input, prog_matlab) %>% 
+    detect_types(!!id, !!input, prog_stata) %>% 
+    detect_types(!!id, !!input, prog_sas) %>% 
+    detect_types(!!id, !!input, prog_julia) 
+  
+  df <- df %>% 
+    as.data.frame() %>% 
+    dplyr::rowwise() %>% 
+    dplyr::mutate(prog_stat_all = sum(across(contains("prog_")), na.rm = TRUE))
+  
+  if( sum_only == TRUE ){
+    
+    df <- df %>% select(-starts_with("prog_"))
+    
+  } else { df }
+  
+  if( prob == TRUE ){
+    
+    tmp_df <- readme_raw_data %>% 
+      as.data.frame() %>% 
+      tidytext::unnest_tokens(word, !!input) %>%
+      as_tidytable() %>% 
+      tidytable::count.(!!id, name = "n_words")
+    
+  } else { df }
+  
+  df
+  
+}
+
+# detect_prog_web_sw classifies web, application and gaming programming languages  
+
+detect_prog_web_sw <- function(df, id, input, sum_only = FALSE, prob = FALSE){
+  
+  library("dplyr")
+  library("readr")
+  library("tidyr")
+  library("readr")
+  library("stringr")
+  library("data.table")
+  library("tidytext")
+  library("tidytable")
+  
+  # load dictionaries for top-50 languages based on: 
+  # https://madnight.github.io/githut/#/pull_requests/2021
+  
+  # top web dev, app, gaming programming languages 
+  prog_javascript <- get_dictionary_terms(sub_type = "JavaScript")
+  prog_java <- get_dictionary_terms(sub_type = "Java")
+  prog_typescript <- get_dictionary_terms(sub_type = "TypeScript")
+  prog_php <- get_dictionary_terms(sub_type = "PHP")
+  prog_html <- get_dictionary_terms(sub_type = "HTML")
+  prog_css <- get_dictionary_terms(sub_type = "CSS")
+  prog_dart <- get_dictionary_terms(sub_type = "Dart") # from google 
+  prog_kotlin <- get_dictionary_terms(sub_type = "Kotlin")
+  prog_lua <- get_dictionary_terms(sub_type = "Lua")
+  prog_coffeescript <- get_dictionary_terms(sub_type = "CoffeeScript")
+  prog_dm <- get_dictionary_terms(sub_type = "DM") # used for games 
+  prog_erlang <- get_dictionary_terms(sub_type = "Erlang") # ecommerce, banking, IMing
+  prog_elixer <- get_dictionary_terms(sub_type = "Elixir") # used with erlang 
+  prog_jsonnet <- get_dictionary_terms(sub_type = "Jsonnet") # structures json data 
+  prog_elm <- get_dictionary_terms(sub_type = "Elm")
+  prog_webassembly <- get_dictionary_terms(sub_type = "WebAssembly")
+  prog_purescript <- get_dictionary_terms(sub_type = "PureScript")
+  
+  id <- enquo(id)
+  input <- enquo(input)
+  
+  df <- df %>% 
+    as_tidytable() %>% 
+    tidytable::mutate.("{{input}}" := tolower({{ input }})) %>% 
+    detect_types(!!id, !!input, prog_javascript) %>% 
+    detect_types(!!id, !!input, prog_java) %>% 
+    detect_types(!!id, !!input, prog_typescript) %>% 
+    detect_types(!!id, !!input, prog_php) %>% 
+    detect_types(!!id, !!input, prog_html) %>% 
+    detect_types(!!id, !!input, prog_css) %>% 
+    detect_types(!!id, !!input, prog_dart) %>% 
+    detect_types(!!id, !!input, prog_kotlin) %>% 
+    detect_types(!!id, !!input, prog_lua) %>% 
+    detect_types(!!id, !!input, prog_coffeescript) %>% 
+    detect_types(!!id, !!input, prog_dm) %>% 
+    detect_types(!!id, !!input, prog_erlang) %>% 
+    detect_types(!!id, !!input, prog_elixer) %>% 
+    detect_types(!!id, !!input, prog_jsonnet) %>% 
+    detect_types(!!id, !!input, prog_elm) %>% 
+    detect_types(!!id, !!input, prog_webassembly) %>% 
+    detect_types(!!id, !!input, prog_purescript) 
+  
+  df <- df %>% 
+    as.data.frame() %>% 
+    dplyr::rowwise() %>% 
+    dplyr::mutate(prog_web_all = sum(across(contains("prog_")), na.rm = TRUE))
+  
+  if( sum_only == TRUE ){
+    
+    df <- df %>% select(-starts_with("prog_"))
+    
+  } else { df }
+  
+  if( prob == TRUE ){
+    
+    tmp_df <- readme_raw_data %>% 
+      as.data.frame() %>% 
+      tidytext::unnest_tokens(word, !!input) %>%
+      as_tidytable() %>% 
+      tidytable::count.(!!id, name = "n_words")
+    
+  } else { df }
+  
+  df
+  
+}
+
+# detect_prog_gen_sw classifies general and multipurpuse use programming languages
+
+detect_prog_gen_sw <- function(df, id, input, sum_only = FALSE, prob = FALSE){
+  
+  library("dplyr")
+  library("readr")
+  library("tidyr")
+  library("readr")
+  library("stringr")
+  library("data.table")
+  library("tidytext")
+  library("tidytable")
+  
+  # load dictionaries for top-50 languages based on: 
+  # https://madnight.github.io/githut/#/pull_requests/2021
+  
+  # top statistical languages 
+  #prog_general <- get_dictionary_terms(summary_type = "Programming")
+  prog_go <- get_dictionary_terms(sub_type = "Go")
+  prog_ruby <- get_dictionary_terms(sub_type = "Ruby")
+  prog_cpp <- get_dictionary_terms(sub_type = "C++")
+  prog_csharp <- get_dictionary_terms(sub_type = "C#")
+  prog_clang <- get_dictionary_terms(sub_type = "C")
+  prog_objc <- get_dictionary_terms(sub_type = "Objective-C")
+  prog_objcpp <- get_dictionary_terms(sub_type = "Objective-C++")
+  prog_rust <- get_dictionary_terms(sub_type = "Rust")
+  prog_perl <- get_dictionary_terms(sub_type = "Perl")
+  prog_swift <- get_dictionary_terms(sub_type = "Swift") # from apple 
+  prog_haskell <- get_dictionary_terms(sub_type = "Haskell")
+  prog_groovy <- get_dictionary_terms(sub_type = "Groovy") 
+  prog_clojure <- get_dictionary_terms(sub_type = "Clojure") 
+  prog_ocaml <- get_dictionary_terms(sub_type = "OCaml")
+  prog_dotnet <- get_dictionary_terms(sub_type = ".NET")
+  prog_vbdotnet <- get_dictionary_terms(sub_type = "Virtual Basic .NET")
+  prog_fsharp <- get_dictionary_terms(sub_type = "F#")
+  prog_fortran <- get_dictionary_terms(sub_type = "Fortran") # from ibm 
+  prog_commonlisp <- get_dictionary_terms(sub_type = "Common Lisp")
+  prog_coq <- get_dictionary_terms(sub_type = "Coq") # formal proof mgmt system
+  
+  id <- enquo(id)
+  input <- enquo(input)
+  
+  df <- df %>% 
+    as_tidytable() %>% 
+    tidytable::mutate.("{{input}}" := tolower({{ input }})) %>% 
+    #detect_types(!!id, !!input, prog_general) %>% 
+    detect_types(!!id, !!input, prog_go) %>% 
+    detect_types(!!id, !!input, prog_ruby) %>% 
+    detect_types(!!id, !!input, prog_cpp) %>% 
+    detect_types(!!id, !!input, prog_csharp) %>% 
+    detect_types(!!id, !!input, prog_clang) %>% 
+    detect_types(!!id, !!input, prog_objc) %>% 
+    detect_types(!!id, !!input, prog_objcpp) %>% 
+    detect_types(!!id, !!input, prog_rust) %>% 
+    detect_types(!!id, !!input, prog_perl) %>% 
+    detect_types(!!id, !!input, prog_swift) %>% 
+    detect_types(!!id, !!input, prog_haskell) %>% 
+    detect_types(!!id, !!input, prog_groovy) %>% 
+    detect_types(!!id, !!input, prog_clojure) %>% 
+    detect_types(!!id, !!input, prog_ocaml) %>% 
+    detect_types(!!id, !!input, prog_dotnet) %>% 
+    detect_types(!!id, !!input, prog_vbdotnet) %>% 
+    detect_types(!!id, !!input, prog_fsharp) %>% 
+    detect_types(!!id, !!input, prog_fortran) %>% 
+    detect_types(!!id, !!input, prog_commonlisp) %>% 
+    detect_types(!!id, !!input, prog_coq) 
+  
+  df <- df %>% 
+    as.data.frame() %>% 
+    dplyr::rowwise() %>% 
+    dplyr::mutate(prog_gen_all = sum(across(contains("prog_")), na.rm = TRUE))
+  
+  if( sum_only == TRUE ){
+    
+    df <- df %>% select(-starts_with("prog_"))
+    
+  } else { df }
+  
+  if( prob == TRUE ){
+    
+    tmp_df <- readme_raw_data %>% 
+      as.data.frame() %>% 
+      tidytext::unnest_tokens(word, !!input) %>%
+      as_tidytable() %>% 
+      tidytable::count.(!!id, name = "n_words")
+    
+  } else { df }
+  
+  df
+  
+}
 
 
 
--- src/02_classify_readmes/04_classify_training_data.R
+++ src/02_classify_readmes/04_classify_training_data.R
@@ -1,4 +1,5 @@
 
+
 # create training sets 
 
 #### load the data and functions #####################################################################################
@@ -8,7 +9,7 @@ library("tidyverse")
 library("dplyr")
 library("readr")
 library("RPostgreSQL")
-source("~/git/dspg21oss/scripts/detect_sw_sz.R")
+#source("~/git/dspg21oss/scripts/detect_sw_sz.R")
 source("~/git/dspg21oss/scripts/detect_sw_co.R")
 source("~/git/dspg21oss/scripts/detect_sw.R")
 
@@ -32,219 +33,56 @@ dbDisconnect(conn)
 
 # repo_stats data 
 setwd(path_for_data)
-repo_stats <- read_csv("repo_stats_0713.csv")
+repo_stats <- read_csv("repo_stats_0714.csv")
 
-readme_subset_data <- readme_raw_data %>% 
-  left_join(repos_table, by = "slug") %>% 
+readme_subset_data <- repos_table  %>% 
   left_join(repo_stats, by = "slug") %>% 
+  left_join(readme_raw_data, by = "slug") %>% 
   select(slug, description, readme_text, language, 
          topics, commits, forks, stars, watchers) %>%
+  arrange(-stars) %>% 
   # clean the topics column 
   mutate(topics = str_replace_all(topics, "\\[", ""),
          topics = str_replace_all(topics, "\\]", ""),
          topics = str_replace_all(topics, "',", ""),
          topics = str_replace_all(topics, "'", "")) %>% 
+  drop_na(description) %>% 
   slice(1:10000) 
+
 
 #### classify with descriptions #####################################################################################
 
 classified_with_desciptions <- readme_subset_data %>% 
   # programming languages 
+  detect_prog_gen_sw(slug, description) %>% 
   detect_prog_stat_sw(slug, description) %>% 
   detect_prog_web_sw(slug, description) %>% 
   # system software 
   detect_system_sw(slug, description) %>% 
   # blockchain technologies
   detect_blockchain_sw(slug, description, sum_only = TRUE) %>%
+  # ai/machine learning 
+  detect_ai_sw(slug, description, sum_only = TRUE) %>%
   # business/office management
   detect_business_sw(slug, description, sum_only = TRUE) %>% 
   # database management
   detect_database_sw(slug, description, sum_only = TRUE) %>% 
   # parse the variables down to what we have finished 
-  select(slug, prog_python, prog_scala, prog_rlang, prog_julia, prog_stat_all,
-         prog_javascript, prog_java, prog_typescript, prog_kotlin, prog_lua, prog_web_all,
+  select(slug, description, readme_text, language, topics, commits, forks, stars, watchers, 
+         prog_python, prog_rlang, prog_java, prog_javascript, prog_php, prog_clang, prog_stat_all,
          sys_windows, sys_linux, sys_mac, sys_android, sys_virtual, starts_with("app_")) %>% 
-  rename_all(paste0, "_desc") %>% 
-  rename(slug = slug_desc)
+  rename(readme = readme_text)
 
-#### classify with coding langs #####################################################################################
+colSums(classified_with_desciptions %>% select(-slug, -description, -readme, -language, -topics))
 
-classified_with_primarylang <- readme_subset_data %>% 
-  # programming languages 
-  detect_prog_stat_sw(slug, language) %>% 
-  detect_prog_web_sw(slug, language) %>% 
-  # system software 
-  detect_system_sw(slug, language) %>% 
-  # database management
-  detect_database_sw(slug, language, sum_only = TRUE) %>% 
-  # blockchain tecnologies
-  detect_blockchain_sw(slug, language, sum_only = TRUE) %>%
-  # business/office management
-  detect_business_sw(slug, language, sum_only = TRUE) %>%
-  # parse the variables down to what we have finished 
-  select(slug, prog_python, prog_scala, prog_rlang, prog_julia, prog_stat_all,
-         prog_javascript, prog_java, prog_typescript, prog_kotlin, prog_lua, prog_web_all,
-         sys_windows, sys_linux, sys_mac, sys_android, sys_virtual, starts_with("app_")) %>% 
-  rename_all(paste0, "_lang") %>% 
-  rename(slug = slug_lang)
+#### subset labeled data #####################################################################################
 
-#### classify with readmes #####################################################################################
-
-classified_with_readmes <- readme_subset_data %>% 
-  # programming languages 
-  detect_prog_stat_sw(slug, readme_text) %>% 
-  detect_prog_web_sw(slug, readme_text) %>% 
-  # system software 
-  detect_system_sw(slug, readme_text) %>% 
-  # database management
-  detect_database_sw(slug, readme_text, sum_only = TRUE) %>% 
-  # blockchain tecnologies
-  detect_blockchain_sw(slug, readme_text, sum_only = TRUE) %>%
-  # business/office management
-  detect_business_sw(slug, readme_text, sum_only = TRUE) %>%
-  # parse the variables down to what we have finished 
-  select(slug, description, readme_text, topics, language, commits, forks, stars, watchers,
-         prog_python, prog_scala, prog_rlang, prog_julia, prog_stat_all,
-         prog_javascript, prog_java, prog_typescript, prog_kotlin, prog_lua, prog_web_all,
-         sys_windows, sys_linux, sys_mac, sys_android, sys_virtual, starts_with("app_")) %>% 
-  rename_all(paste0, "_read") %>% 
-  rename(slug = slug_read)
-
-#### classify with topics #####################################################################################
-
-classified_with_topics <- readme_subset_data %>% 
-  # programming languages 
-  detect_prog_stat_sw(slug, topics) %>% 
-  detect_prog_web_sw(slug, topics) %>% 
-  # system software 
-  detect_system_sw(slug, topics) %>% 
-  # database management
-  detect_database_sw(slug, topics, sum_only = TRUE) %>% 
-  # blockchain tecnologies
-  detect_blockchain_sw(slug, topics, sum_only = TRUE) %>%
-  # business/office management
-  detect_business_sw(slug, topics, sum_only = TRUE) %>%
-  # parse the variables down to what we have finished 
-  select(slug, description, readme_text, topics, language, commits, forks, stars, watchers,
-         prog_python, prog_scala, prog_rlang, prog_julia, prog_stat_all,
-         prog_javascript, prog_java, prog_typescript, prog_kotlin, prog_lua, prog_web_all,
-         sys_windows, sys_linux, sys_mac, sys_android, sys_virtual, starts_with("app_")) %>% 
-  rename_all(paste0, "_tops") %>% 
-  rename(slug = slug_tops)
-
-#### join dataframes #####################################################################################
-
-all_classified_data <- readme_subset_data %>% 
-  left_join(classified_with_desciptions, by = "slug") %>%
-  left_join(classified_with_primarylang, by = "slug") %>% 
-  left_join(classified_with_readmes, by = "slug") %>% 
-  left_join(classified_with_topics, by = "slug") 
-
-#### stat prog langs #####################################################################################
-
-python_data <- all_classified_data %>% 
-  #select(slug, description, readme_text, starts_with("prog_python")) %>% 
-  filter(prog_python_desc == 1 | prog_python_lang == 1 | prog_python_read == 1 | prog_python_tops == 1 ) %>% 
-  #rowwise(slug, description, readme_text) %>% 
-  #summarise(python = sum(c(prog_python_desc, prog_python_lang, prog_python_read, prog_python_tops)))
-  
-colSums(python_data %>% select(-slug, -description, -readme_text))
-
-javascript_data <- all_classified_data %>% 
-  #select(slug, description, readme_text, starts_with("prog_javascript")) %>% 
-  filter(prog_javascript_desc == 1 | prog_javascript_lang == 1 | 
-           prog_javascript_read == 1 | prog_javascript_tops == 1)
-colSums(javascript_data %>% select(-slug, -description, -readme_text))
-
-blockchain_data <- all_classified_data %>% 
-  #select(slug, description, readme_text, starts_with("app_blockchain_all_")) %>% 
-  filter(app_blockchain_all_desc == 1 | app_blockchain_all_lang == 1 | 
-           app_blockchain_all_read == 1 | app_blockchain_all_tops == 1)
-colSums(blockchain_data %>% select(-slug, -description, -readme_text))
-
-android_data <- all_classified_data %>% 
-  #select(slug, description, readme_text, starts_with("sys_android_")) %>% 
-  filter(sys_android_desc == 1 | sys_android_lang == 1 | 
-           sys_android_read == 1 | sys_android_tops == 1 )
-colSums(android_data %>% select(-slug, -description, -readme_text))
-
-business_data <- all_classified_data %>% 
-  #select(slug, description, readme_text, starts_with("sys_android_")) %>% 
-  filter(app_business_all_desc == 1 | app_bus_dsktoppub_lang == 1 | 
-           app_bus_dsktoppub_read == 1 | app_bus_dsktoppub_tops == 1)
-colSums(android_data %>% select(-slug, -description, -readme_text))
-
-temp_data <- bind_rows(python_data, javascript_data, blockchain_data, android_data, business_data) %>% 
-  distinct(slug, description, readme_text, language, commits, forks, stars, watchers, 
-           
-           )
+output_data <- classified_with_desciptions %>% 
+  arrange(-commits) %>% 
+  select(-readme)
 
 setwd(path_for_data)
-write_csv(temp_data, "oss_software_tolabel_071321.csv")
-
-
-## not generating anything yet 
-
-r_data <- all_classified_data %>% 
-  select(slug, description, readme_text, starts_with("prog_rlang")) %>% 
-  filter(prog_rlang_desc == 1 | prog_rlang_lang == 1 | prog_rlang_read == 1 )
-
-colSums(r_data %>% select(-slug, -description, -readme_text))
-
-scala_data <- all_classified_data %>% 
-  select(slug, description, readme_text, starts_with("prog_scala")) %>% 
-  filter(prog_scala_desc == 1 | prog_scala_lang == 1 | prog_scala_read == 1 )
-
-colSums(scala_data %>% select(-slug, -description, -readme_text))
-
-julia_data <- all_classified_data %>% 
-  select(slug, description, readme_text, starts_with("prog_julia")) %>% 
-  filter(prog_julia_desc == 1 | prog_julia_lang == 1 | prog_julia_read == 1 )
-
-colSums(julia_data %>% select(-slug, -description, -readme_text))
-
-java_data <- all_classified_data %>% 
-  select(slug, description, readme_text, starts_with("prog_java")) %>% 
-  filter(prog_java_desc == 1 | prog_java_lang == 1 | prog_java_read == 1 )
-
-colSums(java_data %>% select(-slug, -description, -readme_text))
-
-kotlin_data <- all_classified_data %>% 
-  select(slug, description, readme_text, starts_with("prog_kotlin")) %>% 
-  filter(prog_kotlin_desc == 1 | prog_kotlin_lang == 1 | prog_kotlin_read == 1 )
-
-colSums(kotlin_data %>% select(-slug, -description, -readme_text))
-
-
-#### to do later #####################################################################################
-
-# not ready until after wednesday (july 15th, 2021)
-classified_with_topics %>%
-  # database management  
-  detect_database_sw(slug, readme_text) %>% 
-  # ai/machine learning 
-  detect_aiml_sw(slug, readme_text) %>% 
-  # data visualization 
-  detect_dataviz_sw(slug, readme_text)
-
-
-#### subsetting the training sets #####################################################################################
-
-python_examples <- classified_with_topics %>% 
-  filter(prog_python == 1) %>% 
-  distinct(readme_text)
-
-
-
-
-
-
-
-
-
-
-
-
+write_csv(output_data, "oss_software_tolabel_071421.csv")
 
 
 
