333bbc6e30b54e9294dfb2787770e3e75b9e3f78
Devika Nair <dnair@virginia.edu>
2021-07-28 15:19:22 -0400

    3 modified files from CO brancch

--- src/06_bert/02_clean_descriptions.R
+++ src/06_bert/02_clean_descriptions.R
@@ -4,6 +4,7 @@ library(tidytext)
 library(wordcloud)
 library(stringr)
 
+
 # read in File
 descrips <- read_csv("~/git/dspg21oss/data/dspg21oss/github_repos_157k.csv")
 
@@ -26,9 +27,16 @@ clean_descrips$description <- str_replace_all(clean_descrips$description,"^[0-9]
 # remove non-english characters
 clean_descrips$description <- str_remove_all(clean_descrips$description, "[^[\\da-zA-Z ]]")
 
+# try to identify lang
+clean_descrips$cld3<-lapply(lang$description, cld3::detect_language)
+clean_descrips$cld2<-lapply(lang$description, cld2::detect_language)
+
+clean_descrips <- lang %>% 
+  filter(cld3=="en"|cld2=="en")
+
 # select slug and description
 small_clean_descrips <- clean_descrips %>% 
   select(slug, description)
 
 # save file
-write_csv(small_clean_descrips,"/home/dab3dj/git/dspg21oss/data/dspg21oss/clean_github_repos_157k.csv")
+write_csv(small_clean_descrips,"/home/dab3dj/git/dspg21oss/data/dspg21oss/clean_eng_github_repos_157k.csv")
